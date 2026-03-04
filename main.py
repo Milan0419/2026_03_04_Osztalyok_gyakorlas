7268f2354f84593ec4c9608c63c1117b63e097ae
Milan0419 <balint.milan@diak.szbi-pg.hu>
2026-03-04 07:55:01 +0100

    Heheh

---
+++ main.py
@@ -0,0 +1,9 @@
+from auto import Auto
+
+auto1 = Auto("Toyota", "Corolla", 2015)
+auto2 = Auto("Volkswagen", "Passat", 2002)
+auto3= Auto("Bugatti", "Veyron", 2007)
+
+print(auto1)
+print(auto2)
+print(auto3)
